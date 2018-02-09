aa7eae0d073e5ee7281b46a12ee431f757ed967b
Bill Riley <briley@mmgyglobal.com>
2018-02-08 21:30:09 -0600

    JS send test

--- src/js/global-scripts.js
+++ src/js/global-scripts.js
@@ -7,9 +7,11 @@
 		},
 		addListeners: function() {
 			$('.buttons > a').on('click', main.frameSwap);
+			$('#test').on('click', main.handleOutboundLinkClicks);
 		},
 		removeListeners: function() {
 			$('.buttons > a').off('click', main.frameSwap);
+			$('#test').off('click', main.handleOutboundLinkClicks);
 		},
 		refreshListeners: function() {
 			main.removeListeners();
@@ -17,10 +19,18 @@
 		},
 		frameSwap: function(e) {
 			var el = $(this),
-				target = $(this).parent('.buttons').data('target');
+			target = $(this).parent('.buttons').data('target');
 			$('.frame').removeClass('active');
 			$('#'+target).addClass('active');
 			e.preventDefault();
+		},
+		handleOutboundLinkClicks: function(event) {
+			alert('test');
+			ga('send', 'event', {
+				eventCategory: 'Outbound Link',
+				eventAction: 'click',
+				eventLabel: 'label'
+			});
 		}
 	};
 
